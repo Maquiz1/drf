f2a8053a26f8a63e65f4ae0dde0a2a9e870048c5
Maquiz1 <manquiz92@gmail.com>
2025-12-19 22:35:07 +0300

    Silk Optimizations

--- src/urls.py
+++ src/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('silk/', include('silk.urls', namespace='silk')),
     path('schema-viewer/', include('schema_viewer.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
