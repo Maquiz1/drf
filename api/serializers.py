a291903c85f391b23b07f214fc1df50c03bad019
Maquiz1 <manquiz92@gmail.com>
2025-12-19 21:13:23 +0300

    Nested Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -10,3 +10,24 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be a positive number.")
         return value
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    # product = ProductSerializer(read_only=True)
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2, read_only=True)
+    
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_price', 'quantity', 'item_subtotal')
+        
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+    
+    class Meta:
+        model = Order
+        fields = ('order_id', 'created_at', 'user', 'status','items', 'total_items','total_price')
--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from api import views
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
     path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('orders/', views.order_list, name='order-list'),
+    # path('orders/<uuid:order_id>/', views.order_detail, name='order-detail'),
 ]
