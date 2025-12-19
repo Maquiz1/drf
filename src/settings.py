80f73ac0ed62af9dacbc3a3291fe1a52aaa2f4b1
Maquiz1 <manquiz92@gmail.com>
2025-12-19 17:40:49 +0300

    Serializers and Response

--- src/settings.py
+++ src/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'api',
     'schema_viewer',
     'django_extensions',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/urls.py
+++ src/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('schema-viewer/', include('schema_viewer.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
