70a25e8942a4c7d08d13cae3a10c7d93e09c3d2a
Maquiz1 <manquiz92@gmail.com>
2025-12-19 23:32:44 +0300

    generic views

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product-list'),
-    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('products/', views.ProductListAPIView.as_view(), name='product-list'),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     path('product-info/', views.product_info, name='product-info'),
-    path('orders/', views.order_list, name='order-list'),
+    path('orders/', views.OrderListAPIView.as_view(), name='order-list'),
     # path('orders/<uuid:order_id>/', views.order_detail, name='order-detail'),
 ]
